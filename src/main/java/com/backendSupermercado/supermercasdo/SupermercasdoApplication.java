27bd1799b6398f9879eb83e41f6350d375d102aa
Cayu-Progamador <73171316santicarlos@gmail.com>
2026-04-25 23:09:25 -0400

    hola mundo

--- src/main/java/com/backendSupermercado/supermercasdo/SupermercasdoApplication.java
+++ src/main/java/com/backendSupermercado/supermercasdo/SupermercasdoApplication.java
@@ -2,10 +2,17 @@ package com.backendSupermercado.supermercasdo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SupermercasdoApplication {
 
+	@GetMapping("/saludar")
+	public String saludar () {
+		return "Hola Mundo";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SupermercasdoApplication.class, args);
 	}
