9037e708ee550cced7ba4bd963a6aa928493a35c
Cayu-Progamador <73171316santicarlos@gmail.com>
2026-04-25 22:41:56 -0400

    Primer commit backend Spring Boot

---
+++ src/main/java/com/backendSupermercado/supermercasdo/SupermercasdoApplication.java
@@ -0,0 +1,13 @@
+package com.backendSupermercado.supermercasdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercasdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercasdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendSupermercado/supermercasdo/SupermercasdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendSupermercado.supermercasdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercasdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
